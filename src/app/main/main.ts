d8d443c733d95066effb51ef88402986b2907079
Flyli0 <kirillrozkov653@gmail.com>
2026-06-19 14:46:55 +0500

    Making UI, added buttons and rotated elements

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
+
 export class Main {
+  radius = 1000
+  words = [
+    "About Me", "Education", "Skills", "Projects", "Contacts"
+  ].map((word,index,array) => {
+    if(index%2 == 0){
+      this.radius = 620;
+    }
+    else{
+      this.radius = 600;
+    }
+    const angle = index*40/array.length-15 ;
+    return{
+      word,
+      transform: `rotate(${angle}deg) translateX(${this.radius}px)`,
+    }
+  })
+
 
 }
